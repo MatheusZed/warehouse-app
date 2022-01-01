0e9336b6f4715f8b822ca56f40cea82821134701
Matheus de Oliveira Souza <matheus.oliveira2018@outlook.com.br>
2022-01-01 01:25:09 -0300

    Adicionado validacoes no CEP e codigo unico no nome e codigo

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -2,4 +2,6 @@ class Warehouse < ApplicationRecord
   #Regras de negocio
   validates :name, :code, :description, :address, :state, :city, 
             :postal_code, :total_area,:useful_area, presence: true
+  validates :name, :code, uniqueness: true
+  validates :postal_code, format: { with: /(^\d{5}-\d{3}$)/, message: "não é valido, formato: 00000-000"}
 end
