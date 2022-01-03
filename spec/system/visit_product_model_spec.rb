587197bbb139160c1d861e2bb428ebb6b4499a36
Matheus de Oliveira Souza <matheus.oliveira2018@outlook.com.br>
2022-01-03 01:16:56 -0300

    Ajustes mais um novo teste

--- spec/system/visit_product_model_spec.rb
+++ spec/system/visit_product_model_spec.rb
@@ -24,23 +24,22 @@ describe 'Visitor sees the product model' do
     expect(page).to have_content 'Fornecedor: Joao'
   end
 
-  #O problema desse teste e que a rota ao retornar se perde pq 'e para ir para outro caminho
-  # it 'and can return to suppliers page' do
-  #   # Arrange
-  #   s = Supplier.create!(fantasy_name: 'Joao', legal_name: 'Joao pe de feijao', 
-  #                        cnpj: '30605809000108', address: 'Av Fernandes Lima', 
-  #                        email: 'joao.feijao@yahoo.com', phone: '91124-7753')
-  #   ProductModel.create!(name: 'Saco de Feijao', weight: 1000, height: 4,
-  #                        width: 17, length: 22, supplier: s, sku: 'I12A')
+  it 'and can return to suppliers page' do
+    # Arrange
+    s = Supplier.create!(fantasy_name: 'Joao', legal_name: 'Joao pe de feijao', 
+                         cnpj: '30605809000108', address: 'Av Fernandes Lima', 
+                         email: 'joao.feijao@yahoo.com', phone: '91124-7753')
+    ProductModel.create!(name: 'Saco de Feijao', weight: 1000, height: 4,
+                         width: 17, length: 22, supplier: s, sku: 'I12A')
 
-  #   # Act
-  #   visit root_path
-  #   click_on 'See suppliers'
-  #   click_on 'Joao'
-  #   click_on 'Saco de Feijao'
-  #   click_on 'Return'
+    # Act
+    visit root_path
+    click_on 'See suppliers'
+    click_on 'Joao'
+    click_on 'Saco de Feijao'
+    click_on 'Return'
 
-  #   # Assert
-  #   expect(current_path).to eq supplier_path
-  # end
+    # Assert
+    expect(current_path).to eq supplier_path(s.id)
+  end
 end
